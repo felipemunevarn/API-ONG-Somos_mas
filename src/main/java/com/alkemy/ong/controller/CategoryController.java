49aeb74efd54bdfaae85579e557942547d3029c5
FelipeMunevar <felipemunevarn@gmail.com>
2022-10-20 20:24:07 -0300

    trying not to build

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import javax.validation.Valid;
 
-@RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
